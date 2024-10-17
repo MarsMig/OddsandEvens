faa047d316b6ea82b2fa428c41c7a3ff5fbb8456
MarsMig <mars.n.m@outlook.com>
2024-10-17 11:33:44 -0400

    created first piece of forum code, still not showing up in number bank or console

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // TODO: this file! :)
+const form = document.querySelector("form");
+const section = document.getElementsByClassName("section")[0];
+
+let number = [];
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  number.push(data.get("number"));
+});
+printAllNumber = () => {
+  const input = document.querySelector("input");
+  input.innerText = number;
+};
