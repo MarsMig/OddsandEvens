9c534c9f584e3d9a3cb81ee1abd7ec527d027cc1
MarsMig <mars.n.m@outlook.com>
2024-10-19 09:18:36 -0400

    final commit, working forum used javascript to seperate number array into odds and evens by singular number and whole array, final commit

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
 // TODO: this file! :)
 const form = document.querySelector("form");
-const section = document.getElementsByClassName("section")[0];
+const sortOne = document.getElementById("sortOne");
+const sortAll = document.getElementById("sortAll");
 
 let number = [];
+let even = [];
+let odd = [];
+const printAllNumber = () => {
+  const output = document.querySelector("output");
+  output.innerText = number;
+};
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   const data = new FormData(event.target);
-  number.push(data.get("number"));
+  if (data.get("number") !== "") {
+    number.push(data.get("number"));
+    printAllNumber();
+    form.reset();
+  }
 });
-printAllNumber = () => {
-  const input = document.querySelector("input");
-  input.innerText = number;
-};
+sortOne.addEventListener("click", function (event) {
+  let num = number.shift();
+  if (num % 2 !== 0) {
+    const output = document.querySelector("#odds output");
+    odd.push(num);
+    output.innerText = odd;
+  } else {
+    const output = document.querySelector("#evens output");
+    even.push(num);
+    output.innerText = even;
+  }
+  printAllNumber();
+});
+sortAll.addEventListener("click", function (event) {
+  while (number.length > 0) {
+    let num = number.shift();
+    if (num % 2 !== 0) {
+      const output = document.querySelector("#odds output");
+      odd.push(num);
+      output.innerText = odd;
+    } else {
+      const output = document.querySelector("#evens output");
+      even.push(num);
+      output.innerText = even;
+    }
+    printAllNumber();
+  }
+});
